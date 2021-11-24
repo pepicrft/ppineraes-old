b3938415159f1fed0711868fe25d98254d5e4bbd
Pedro Piñera <pepibumur@gmail.com>
2021-11-24 09:17:25 +0100

    Pass a list of posts to pre-render

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,18 @@ import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-netlify';
+import glob from "glob-fs";
+
+const postSlugs = glob({gitignore: true}).readdirSync("posts/*.md")
+	.map((postFilePath) => {
+		// posts/2021-02-02-my-post.md
+		const components = postFilePath.split("/")[1].split("-")
+		const year = components[0]
+		const month = components[1]
+		const day = components[2]
+		const name = components.slice(3).join("-").replace(".md", "")
+		return `/${year}/${month}/${day}/${name}`
+	})
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -16,6 +28,12 @@ const config = {
 		target: '#svelte',
 		files: {
 			assets: 'static'
+		},
+		prerender: {
+			crawl: true,
+			entries: [
+				...postSlugs
+			]
 		}
 	}
 };
