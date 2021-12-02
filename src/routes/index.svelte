61d44896cc2d0b662bab2bb3859eba1d8fcfa15e
Pedro Piñera <pedro@ppinera.es>
2021-12-02 08:43:53 +0100

    Fix some links

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -54,8 +54,8 @@
 		<ul>
 			<li><a href="/about">About</a></li>
 			<li><a href="/stack">Stack</a></li>
-			<li><a href="/bookshelve">Bookshelf</a></li>
-			<li><a href="/bookshelve">Micro-posts</a></li>
+			<li><a href="/bookshelf">Bookshelf</a></li>
+			<li><a href="/micro-posts">Micro-posts</a></li>
 			<li><a href="https://www.instapaper.com/p/6377648" target="_blank">Read later</a></li>
 		</ul>
 		<li>All posts</li>
