8930e39908d83b4787d674afc1d11aeb2d1d0ccc
Pedro Piñera <pedro@ppinera.es>
2021-12-02 08:48:33 +0100

    Allow navigation to posts

--- src/routes/posts/__layout.svelte
+++ src/routes/posts/__layout.svelte
@@ -1,7 +1,35 @@
-<script>
-	export let title;
-	export let author;
-	export let date;
+<script context="module">
+	export const prerender = true;
+
+	export async function load({ page, fetch, session, stuff }) {
+		const url = `/posts.json`;
+		const res = await fetch(url);
+
+		if (res.ok) {
+			return {
+				props: {
+					posts: (await res.json()).slice(0, 5)
+				}
+			};
+		}
+
+		return {
+			status: res.status,
+			error: new Error(`Could not load ${url}`)
+		};
+	}
 </script>
+
+<script>
+	import { page } from '$app/stores';
+
+	const path = $page.path;
+	export let posts = [];
+	const title = posts.find((post) => post.slug.includes(path)).title;
+</script>
+
+<h1>
+	{title}
+</h1>
 
 <slot />
